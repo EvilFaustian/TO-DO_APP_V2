9aacbbeae16ec21d04163ea1df2e813193364cf9
EvilFaustian <101469392+EvilFaustian@users.noreply.github.com>
2022-03-21 02:47:56 +0200

    Update TodoApp.vue

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -4,116 +4,142 @@
 
     <!-- Input -->
     <div class="d-flex">
-      <input v-model="this.$store.state.task" type="text" class="form-control" placeholder="Enter new TODO task">
-      <button @click="submitTask" class="btn btn-warning" rounded-0>Create</button>
+      <input
+        v-model="this.$store.state.task"
+        type="text"
+        class="form-control"
+        placeholder="Enter new TODO task"
+        @keyup.enter="submitTask"
+      />
+      <button @click="submitTask" class="btn btn-warning" rounded-0>
+        Create
+      </button>
     </div>
     <div class="d-flex justify-content-center">
-      <span class="usernameSpan">User: {{this.$store.state.username}}</span>
+      <span class="usernameSpan">User: {{ this.$store.state.username }}</span>
     </div>
-    
-    <!-- Task table -->
-      <table class="table table-bordered mt-5">
-    <thead>
-      <tr>
-        <th scope="col">Task</th>
-        <th scope="col">Status</th>
-        <th scope="col" class="text-center">#</th>
-        <th scope="col" class="text-center">#</th>
-        <th scope="col" class="text-center">#</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(task, index) in todosFiltered" v-bind:key="index" :task="task">
-        <th id="nameContainer">
-          <span :class="{'finished': task.status === 'finished'}">
-            <span class="byAuthor">By {{task.author}}</span>
-            <span class="taskName">{{task.name}}</span>
-            
-            </span>
-            </th>
-        <td style="width: 120px">
-          <span @click="changeStatus(index)" class="pointer"
-          :class="{'text-danger' : task.status === 'to-do',
-          'text-warning' : task.status === 'in-progress',
-          'text-success' : task.status === 'finished'
-          }"
-          >
-            {{firstCharUpper(task.status)}}
-            </span>
-            </td>
-        <td>
-          <div class="text-center" @click="editTask(index)">
-            <span class="fa fa-pen"></span>
-          </div>
-        </td>
-        <td>
-          <div class="text-center" @click="deleteTask(index)">
-            <span class="fa fa-trash"></span>
-          </div>
-        </td>
-        <td>
-          <div class="text-center" @click="archiveTask(index)">
-            <span class="fa fa-save"></span>
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
 
-<table class="table">
-  <tbody>
-    <tr>
-      <td>
+    <!-- Task table -->
+    <table class="table table-bordered mt-5">
+      <thead>
+        <tr>
+          <th scope="col">Task</th>
+          <th scope="col">Status</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+          <th scope="col" class="text-center">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(task, index) in todosFiltered" v-bind:key="index">
+          <th id="nameContainer">
+            <span :class="{ finished: task.status === 'finished' }">
+              <span class="byAuthor">By {{ task.author }}</span>
+              <span class="taskName">{{ task.name }}</span>
+            </span>
+          </th>
+          <td style="width: 120px">
+            <span
+              @click="changeStatus(index)"
+              class="pointer"
+              :class="{
+                'text-danger': task.status === 'to-do',
+                'text-warning': task.status === 'in-progress',
+                'text-success': task.status === 'finished',
+              }"
+            >
+              {{ firstCharUpper(task.status) }}
+            </span>
+          </td>
+          <td>
+            <div class="text-center" @click="editTask(index)">
+              <span class="fa fa-pen"></span>
+            </div>
+          </td>
+          <td>
+            <div class="text-center" @click="deleteTask(index)">
+              <span class="fa fa-trash"></span>
+            </div>
+          </td>
+          <td>
+            <div class="text-center" @click="archiveTask(index)">
+              <span class="fa fa-save"></span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <table class="table">
+      <tbody>
+        <tr>
+          <td>
             <table style="width: 100px" class="table table-bordered">
               <tbody>
                 <tr>
-                  <td><span :class="{ active: this.$store.state.filter == 'all'}" @click="this.$store.state.filter = 'all'">All</span></td>
-                  <td><span :class="{ active: this.$store.state.filter == 'all'}" @click="this.$store.state.filter = 'active'">Active</span></td>
-                  <td><span :class="{ active: this.$store.state.filter == 'all'}" @click="this.$store.state.filter = 'completed'">Completed</span></td>
+                  <td>
+                    <span
+                      :class="{ active: this.$store.state.filter == 'all' }"
+                      @click="this.$store.state.filter = 'all'"
+                      >All</span
+                    >
+                  </td>
+                  <td>
+                    <span
+                      :class="{ active: this.$store.state.filter == 'all' }"
+                      @click="this.$store.state.filter = 'active'"
+                      >Active</span
+                    >
+                  </td>
+                  <td>
+                    <span
+                      :class="{ active: this.$store.state.filter == 'all' }"
+                      @click="this.$store.state.filter = 'completed'"
+                      >Completed</span
+                    >
+                  </td>
                 </tr>
               </tbody>
             </table>
-      </td>
-      <td>
-        <table class="table">
-          <tbody>
-            <tr>
-              <td class="text-center">{{remaining}} task left!</td>
-            </tr>
-          </tbody>
-        </table>
-      </td>
-    </tr>
-  </tbody>
-</table>
-
-
-
+          </td>
+          <td>
+            <table class="table">
+              <tbody>
+                <tr>
+                  <td class="text-center">{{ remaining }} task left!</td>
+                </tr>
+              </tbody>
+            </table>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
   methods: {
     submitTask() {
-      if(this.$store.state.task.length === 0) return;
+      if (this.$store.state.task.length === 0) return;
 
-      if(this.$store.state.editedTask === null){
+      if (this.$store.state.editedTask === null) {
         this.$store.state.tasks.push({
           name: this.$store.state.task,
-          status: 'to-do',
+          status: "to-do",
           author: this.$store.state.username,
-          archived: false
-        })
-      }else {
-        this.$store.state.tasks[this.$store.state.editedTask].name = this.$store.state.task;
+          archived: false,
+        });
+      } else {
+        this.$store.state.tasks[this.$store.state.editedTask].name =
+          this.$store.state.task;
         this.$store.state.editedTask = null;
       }
-      this.$store.state.task = '';
+      this.$store.state.task = "";
     },
     deleteTask(index) {
       this.$store.state.tasks.splice(index, 1);
@@ -125,41 +151,49 @@ export default {
     archiveTask(index) {
       this.$store.state.tasks[index].archived = true;
     },
-    changeStatus(index){
-      let newIndex = this.$store.state.availableStatuses.indexOf(this.$store.state.tasks[index].status);
-      if(++newIndex > 2) newIndex = 0;
-      this.$store.state.tasks[index].status = this.$store.state.availableStatuses[newIndex];
+    changeStatus(index) {
+      let newIndex = this.$store.state.availableStatuses.indexOf(
+        this.$store.state.tasks[index].status
+      );
+      if (++newIndex > 2) newIndex = 0;
+      this.$store.state.tasks[index].status =
+        this.$store.state.availableStatuses[newIndex];
     },
     firstCharUpper(str) {
       return str.charAt(0).toUpperCase() + str.slice(1);
     },
     debug() {
-      alert(this.$store.state.task)
+      alert(this.$store.state.task);
     },
   },
   computed: {
     todosFiltered() {
-          if (this.$store.state.filter == 'all') {
-              return this.$store.state.tasks.filter(task => task.archived == false)
-          } else if (this.$store.state.filter == 'active') {
-              return this.$store.state.tasks.filter(task => task.status != "finished" && task.archived == false)
-          } else if (this.$store.state.filter == 'completed') {
-              return this.$store.state.tasks.filter(task => task.status == "finished"  && task.archived == false)
-          }
-          return this.$store.state.tasks
-      },
-      remaining() {
-          return this.$store.state.tasks.filter(task => task.status != "finished" && !task.archived).length
-      },
-      archiveTodoFilter() {
-        return this.$store.state.tasks.filter(task => task.archived == false)
+      if (this.$store.state.filter == "all") {
+        return this.$store.state.tasks.filter((task) => !task.archived);
+      } else if (this.$store.state.filter == "active") {
+        return this.$store.state.tasks.filter(
+          (task) => task.status != "finished" && !task.archived
+        );
+      } else if (this.$store.state.filter == "completed") {
+        return this.$store.state.tasks.filter(
+          (task) => task.status === "finished" && !task.archived
+        );
       }
-  }
+      return this.$store.state.tasks.filter((task) => !task.archived);
+    },
+    remaining() {
+      return this.$store.state.tasks.filter(
+        (task) => task.status != "finished" && !task.archived
+      ).length;
+    },
+    archiveTodoFilter() {
+      return this.$store.state.tasks.filter((task) => !task.archived);
+    },
+  },
 };
 </script>
 
 <style>
-
 .pointer {
   cursor: pointer;
 }
@@ -174,7 +208,7 @@ export default {
 }
 
 .byAuthor {
-  position:relative;
+  position: relative;
   margin-right: -50px;
   top: -20px;
   font-style: normal;
@@ -187,5 +221,4 @@ export default {
   padding: 10px;
   padding-top: 15px;
 }
-
 </style>
